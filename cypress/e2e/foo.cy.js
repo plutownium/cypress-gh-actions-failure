50c88d253f19fde3a01562dba6f779fcd333f593
Roland Mackintosh <rolandmackintosh@outlook.com>
2023-03-17 14:28:22 -0700

    code works locally

--- cypress/e2e/foo.cy.js
+++ cypress/e2e/foo.cy.js
@@ -1,14 +1,10 @@
 describe('Some Test', () => {
-    it('fails when trying to get the btn state!', () => {
+    it('correctly states that the btn is enabled', () => {
       cy.visit("localhost:5173")
       cy.get("h1").should("exist")
       cy.get("button").should("not.be.enabled")
       cy.get("input").type("cat, hat, jazz")
-      cy.wait(500).get("button").should("be.enabled")
-    })
-    it("can find a p tag", () => {
-      
-      cy.visit("localhost:5173")
-      cy.get("p").should("exist")
+      // cy.wait(500)
+      cy.get("button").should("be.enabled")
     })
   })
